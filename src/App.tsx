a51735b2a061a5a02745a277891af3d588594bb1
Omar <omarucolombari@gmail.com>
2022-09-15 15:55:34 -0300

    Feat: feito retangulo abaixo da lista de games e estilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+
 import "./styles/main.css";
 import logoImg from "./assets/logo-nlw-esport.svg";
 
@@ -73,6 +75,23 @@ function App() {
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
         </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] py-6 px-8 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl font-black text-white block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 rounded text-white hover:bg-violet-600 flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   );
