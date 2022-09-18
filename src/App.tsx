81b035a2d8a57fa17022e041ee62a3ae3012ebfb
Omar <omarucolombari@gmail.com>
2022-09-18 16:02:50 -0300

    Feat: criado page Home e integrado com a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import React from "react";
-
+import { Home } from "./pages/Home";
 import "./styles/main.css";
-import logoImg from "./assets/logo-nlw-esport.svg";
-import { GameBanner } from "./components/GameBanner";
-import { CreateAdBanner } from "./components/CreateAdBanner";
 
 const App: React.FC = () => {
   return (
-    <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
-      <img src={logoImg} alt="NLW eSports" />
-      <h1 className="text-6xl text-white font-black mt-20">
-        Seu{" "}
-        <span className="text-transparent bg-nlw-gradient bg-clip-text">
-          duo
-        </span>{" "}
-        está aqui.
-      </h1>
-
-      <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          title="League of Legends"
-          adsCount={1}
-        />
-      </div>
-
-      <CreateAdBanner />
-    </div>
+    <>
+      <Home />
+    </>
   );
 };
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,50 @@
+import logoImg from "../../assets/logo-nlw-esport.svg";
+import { GameBanner } from "../../components/GameBanner";
+import { CreateAdBanner } from "../../components/CreateAdBanner";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+interface IGame {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
+export const Home: React.FC = () => {
+  const [games, setGames] = useState<IGame[]>([]);
+
+  useEffect(() => {
+    api.get<IGame[]>("/games").then((res) => setGames(res.data));
+  }, []);
+
+  return (
+    <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="NLW eSports" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-5 mt-16 w-11/12">
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              adsCount={game._count.ads}
+              bannerUrl={game.bannerUrl}
+            />
+          );
+        })}
+      </div>
+
+      <CreateAdBanner />
+    </div>
+  );
+};
